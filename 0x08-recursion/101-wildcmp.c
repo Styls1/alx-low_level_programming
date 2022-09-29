8c9d4e2f99526c1610d10a794eb96ccea527cddf
Styls1 <bellostyls32162@gmail.com>
2022-09-29 01:11:20 +0000

    Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings and returns 1 if the strings can be
+ * considered identical.
+ *
+ * @s1: The first string.
+ * @s2: The second string contains special character *.
+ *
+ * Return: 1 or otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
+}
