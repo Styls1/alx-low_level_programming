298881fcd0c35f29f645504721a06318d2088ff4
Styls1 <bellostyls32162@gmail.com>
2022-10-06 21:33:28 +0000

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - A program that multiplies two positive numbers.
@@ -11,8 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+unsigned long mul;
+int i, j;
 
 	if (argc != 3)
 	{
@@ -32,5 +32,5 @@ int main(int argc, char *argv[])
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
-	return (0);
+return (0);
 }
