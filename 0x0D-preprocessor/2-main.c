ecf127a020fe99fcaf5212c2076df45ad04fd71c
Styls1 <bellostyls32162@gmail.com>
2022-10-10 19:44:06 +0000

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from,
+ * follwed by a new line.
+ *
+ * Return: Nothing.
+ */
+
+int main(void)
+{
+printf("2-main.c\n");
+return (0);
+}
