97d5e097b1fef2f231abae54694512a7f2b5dcdf
Styls1 <bellostyls32162@gmail.com>
2022-10-10 21:37:29 +0000

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,24 +1,10 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
-#include <stdio.h>
+#define ABS(x) ((x) < (X) ? (X) : (x))
 
 /**
- * main - Macro ABS(x) that computes the absolute value
+ * ABS - Macro ABS(x) that computes the absolute value
  * of a number x.
- * @x: Number to be computed.
- *
- * Return: Nothing.
  */
 
-#define ABS(x) x
-
-int main(void)
-{
-	int i;
-
-	i = ABS(9);
-	printf("%d\n", i);
-
-	return (0);
-}
 #endif /* #ifndef _3_FUNCTION_LIKE_MACRO_H_ */
