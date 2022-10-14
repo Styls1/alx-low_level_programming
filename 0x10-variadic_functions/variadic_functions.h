07c748039c2ba266bd8bf1090c81b9ac06b4cdd9
Styls1 <bellostyls32162@gmail.com>
2022-10-14 14:47:52 +0000

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,6 @@
  * void prototypes (void).
  */
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
