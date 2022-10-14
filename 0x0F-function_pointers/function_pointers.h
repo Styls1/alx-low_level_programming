e0f2aa79e7a2dcc11b71699db64f2b14de1ae723
Styls1 <bellostyls32162@gmail.com>
2022-10-14 16:49:39 +0000

    What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,7 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
-#include <stdlib.h>
 
-/**
- * main - Header file for prototypes.
- * int prototypes (int).
- * void prototypes (void).
- */
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
