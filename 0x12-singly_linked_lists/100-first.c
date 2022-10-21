46524318581ea9333cdd61d22f82e56d1125a290
Styls1 <bellostyls32162@gmail.com>
2022-10-21 00:10:55 +0000

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,16 +13,3 @@ void before_main(void)
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
-/**
- * main - Function to be executed after the before_main.
- *
- * Return: Nothing.
- */
-
-int main(void)
-{
-	printf("The goal is to become a magnificent software engineer!\n");
-
-	return (0);
-}
